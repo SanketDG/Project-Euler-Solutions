39071242ddb36ced30b43b22d7b38c91af662874
SanketDG <sanketdasgupta@gmail.com>
2015-02-26 23:41:01 +0530

    update problem 4

--- 1-10/largest_palindrome_product.py
+++ 1-10/largest_palindrome_product.py
@@ -6,12 +6,11 @@ def isPalin(s):
 for _ in xrange(int(raw_input())):
     s = raw_input()
     n = int(s)
-    max = 0
-    for i in range(999, 100, -1):
-        for j in range(i, 100, -1):
+    maximum = 1
+    for i in xrange(999, 100, -1):
+        for j in xrange(i, 100, -1):
             palin = i * j
-            if isPalin(palin) and palin < n:
-                if palin > max:
-                    max = palin
-                    break
-    print max
+            if isPalin(palin) and palin < n and palin > maximum:
+                maximum = palin
+                break
+    print maximum
