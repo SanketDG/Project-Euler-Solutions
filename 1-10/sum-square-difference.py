2d04a7d9bb4587e98a990e47d22604aa650701da
SanketDG <sanketdasgupta@gmail.com>
2015-02-26 23:42:40 +0530

    pep8 compliant

--- 1-10/sum-square-difference.py
+++ 1-10/sum-square-difference.py
@@ -1,9 +1,12 @@
-"""A palindromic number reads the same both ways. The smallest 6 digit palindrome made from the product of two 3-digit numbers is 101101=143×707.
+"""A palindromic number reads the same both ways. The smallest 6 digit
+palindrome made from the product of two 3-digit numbers is 101101=143×707.
 
-Find the largest palindrome made from the product of two 3-digit numbers which is less than N.
+Find the largest palindrome made from the product of two 3-digit numbers which
+is less than N.
 
 Input Format
-First line contains T that denotes the number of test cases. This is followed by T lines, each containing an integer, N.
+First line contains T that denotes the number of test cases. This is followed
+by T lines, each containing an integer, N.
 
 Output Format
 Print the required answer for each test case in a new line.
